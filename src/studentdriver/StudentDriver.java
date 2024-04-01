7368ae6af28c0bd570479a3245f07916f1e89a41
PierceWand <s563917@nwmissouri.edu>
2024-04-01 11:48:27 -0500

    test
    testing

--- src/studentdriver/StudentDriver.java
+++ src/studentdriver/StudentDriver.java
@@ -16,6 +16,8 @@ public class StudentDriver {
      */
     public static void main(String[] args) {
         System.out.println("Project02");
+        
+        System.out.println("its working");
     }
 
 }
